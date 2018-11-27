9c85f0fd7dfb067a7923136f34856accf159edb6
Junjie Chen <chen.bys@outlook.com>
2018-11-27 15:45:53 +0800

    m

--- train_test_grid_1080.sh
+++ train_test_grid_1080.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-python experiments/rfcn/rfcn_end2end_train_test.py --cfg experiments/rfcn/cfgs/1080ti/0-100-0.yaml
-python experiments/rfcn/rfcn_end2end_train_test.py --cfg experiments/rfcn/cfgs/1080ti/0-10-0.yaml
-python experiments/rfcn/rfcn_end2end_train_test.py --cfg experiments/rfcn/cfgs/1080ti/0-1-0.yaml
-python experiments/rfcn/rfcn_end2end_train_test.py --cfg experiments/rfcn/cfgs/1080ti/0-0.1-0.yaml
+python experiments/rfcn/rfcn_end2end_train_test.py --cfg experiments/rfcn/cfgs/1080/0-100000-0.yaml
+python experiments/rfcn/rfcn_end2end_train_test.py --cfg experiments/rfcn/cfgs/1080/0-10000-0.yaml
+python experiments/rfcn/rfcn_end2end_train_test.py --cfg experiments/rfcn/cfgs/1080/0-1000-0.yaml
+python experiments/rfcn/rfcn_end2end_train_test.py --cfg experiments/rfcn/cfgs/1080/0-0.01-0.yaml
