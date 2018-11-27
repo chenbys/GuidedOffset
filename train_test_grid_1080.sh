377f8f909ae7171aa7dc80b70d21ab5ffaefc515
Junjie Chen <chen.bys@outlook.com>
2018-11-27 15:37:12 +0800

    add cfg 1080

---
+++ train_test_grid_1080.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+python experiments/rfcn/rfcn_end2end_train_test.py --cfg experiments/rfcn/cfgs/1080ti/0-100-0.yaml
+python experiments/rfcn/rfcn_end2end_train_test.py --cfg experiments/rfcn/cfgs/1080ti/0-10-0.yaml
+python experiments/rfcn/rfcn_end2end_train_test.py --cfg experiments/rfcn/cfgs/1080ti/0-1-0.yaml
+python experiments/rfcn/rfcn_end2end_train_test.py --cfg experiments/rfcn/cfgs/1080ti/0-0.1-0.yaml
