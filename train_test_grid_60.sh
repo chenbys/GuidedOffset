2dc615f1c2386147b9c686f401115dd7a0d0b57f
Junjie Chen <chen.bys@outlook.com>
2018-12-05 08:58:09 +0800

    m

--- train_test_grid_60.sh
+++ train_test_grid_60.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-python experiments/rfcn/rfcn_end2end_train_test.py --cfg experiments/rfcn/cfgs/60/0.1-0.yaml
-python experiments/rfcn/rfcn_end2end_train_test.py --cfg experiments/rfcn/cfgs/60/5-0.yaml
-python experiments/rfcn/rfcn_end2end_train_test.py --cfg experiments/rfcn/cfgs/60/1-10.yaml
-python experiments/rfcn/rfcn_end2end_train_test.py --cfg experiments/rfcn/cfgs/60/1-50.yaml
+python experiments/rfcn/rfcn_end2end_train_test.py --cfg experiments/rfcn/cfgs/60/r_0.15.yaml
+python experiments/rfcn/rfcn_end2end_train_test.py --cfg experiments/rfcn/cfgs/60/r_0.05.yaml
